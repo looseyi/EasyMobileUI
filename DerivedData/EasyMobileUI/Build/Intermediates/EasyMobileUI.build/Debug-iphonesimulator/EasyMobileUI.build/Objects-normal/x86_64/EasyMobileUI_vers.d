964c28543028421aa4d1b45a7b7b8f62fc4d59dc
looseyi <13615033587@126.com>
2017-05-22 17:33:11 +0800

    update EasyMobileUI.h file update carthage builds

--- DerivedData/EasyMobileUI/Build/Intermediates/EasyMobileUI.build/Debug-iphonesimulator/EasyMobileUI.build/Objects-normal/x86_64/EasyMobileUI_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/mac/Desktop/EasyMobileUI/DerivedData/EasyMobileUI/Build/Intermediates/EasyMobileUI.build/Debug-iphonesimulator/EasyMobileUI.build/DerivedSources/EasyMobileUI_vers.c
--- EasyMobileUI/EasyMobileUI.h
+++ EasyMobileUI/EasyMobileUI.h
@@ -17,3 +17,29 @@ FOUNDATION_EXPORT const unsigned char EasyMobileUIVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <EasyMobileUI/PublicHeader.h>
 
 
+#import <EasyMobileUI/EaseConversationListViewController.h>
+#import <EasyMobileUI/EaseMessageViewController.h>
+#import <EasyMobileUI/EaseUsersListViewController.h>
+#import <EasyMobileUI/EaseViewController.h>
+
+#import <EasyMobileUI/IModelCell.h>
+#import <EasyMobileUI/IModelChatCell.h>
+#import <EasyMobileUI/EaseMessageCell.h>
+#import <EasyMobileUI/EaseBaseMessageCell.h>
+#import <EasyMobileUI/EaseBubbleView.h>
+#import <EasyMobileUI/EaseUserCell.h>
+
+
+#import <EasyMobileUI/EaseChineseToPinyin.h>
+#import <EasyMobileUI/EaseEmoji.h>
+#import <EasyMobileUI/EaseEmotionEscape.h>
+#import <EasyMobileUI/EaseEmotionManager.h>
+#import <EasyMobileUI/EaseSDKHelper.h>
+#import <EasyMobileUI/EaseConvertToCommonEmoticonsHelper.h>
+
+#import <EasyMobileUI/NSDate+Category.h>
+#import <EasyMobileUI/NSString+Valid.h>
+#import <EasyMobileUI/UIImageView+EMWebCache.h>
+#import <EasyMobileUI/UIViewController+HUD.h>
+#import <EasyMobileUI/UIViewController+DismissKeyboard.h>
+#import <EasyMobileUI/EaseLocalDefine.h>
