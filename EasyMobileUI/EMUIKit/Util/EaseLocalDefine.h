a07dd9a962dee7d5a5b2f03b121d867b8ab33598
looseyi <13615033587@126.com>
2017-05-24 14:41:45 +0800

    fix EasyMobile Localized string bundle mistake

--- EasyMobileUI/EMUIKit/Util/EaseLocalDefine.h
+++ EasyMobileUI/EMUIKit/Util/EaseLocalDefine.h
@@ -10,9 +10,14 @@
  * from Hyphenate Inc.
  */
 
+#import "EaseMessageCell.h"
+
 #ifndef EaseLocalDefine_h
 #define EaseLocalDefine_h
 
-#define NSEaseLocalizedString(key, comment) [[NSBundle bundleWithURL:[[NSBundle mainBundle] URLForResource:@"EaseUIResource" withExtension:@"bundle"]] localizedStringForKey:(key) value:@"" table:nil]
+#define EaseBundlePath [[NSBundle bundleForClass: [EaseMessageCell class]] pathForResource:@"EaseUIResource" ofType:@"bundle"]
+
+#define NSEaseLocalizedString(key, comment) \
+[[NSBundle bundleWithPath:EaseBundlePath] localizedStringForKey:(key) value:@"" table:nil]
 
 #endif /* EaseLocalDefine_h */
